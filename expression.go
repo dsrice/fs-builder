689576b83420947400c5ad454bf1ede18802507e
dsrice <v.fuji.0223@gmail.com>
2023-12-04 02:12:34 +0900

    update: 複数条件の時に取りまわせるよう修正

--- expression.go
+++ expression.go
@@ -1,9 +1,13 @@
 package fsb
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Expression struct {
-	condition string
+	condition  string
+	bracketFlg bool
 }
 
 // Eq
@@ -20,4 +24,30 @@ func Eq(target string, comp interface{}) *Expression {
 	return &Expression{
 		condition: cond,
 	}
+}
+
+// AND
+// AND条件設定
+func (e *Expression) AND(exp *Expression) *Expression {
+	if strings.Contains(exp.condition, "OR") {
+		e.condition = fmt.Sprintf("%s AND (%s)", e.condition, exp.condition)
+	} else {
+		e.condition = fmt.Sprintf("%s AND %s", e.condition, exp.condition)
+	}
+
+	return e
+}
+
+// OR
+// OR条件設定
+func (e *Expression) OR(exp *Expression) *Expression {
+	if strings.Contains(exp.condition, "AND") {
+		e.condition = fmt.Sprintf("%s OR (%s)", e.condition, exp.condition)
+	} else {
+		e.condition = fmt.Sprintf("%s OR %s", e.condition, exp.condition)
+	}
+
+	e.bracketFlg = true
+
+	return e
 }
