d6c78f85bd1d901085ccb281257f3844597f2963
dsrice <v.fuji.0223@gmail.com>
2023-12-04 01:45:44 +0900

    update: godoc追加とローカル環境でのgolangci-lint稼働の修正

--- select.go
+++ select.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 )
 
+// SelectContainer
+// Select関連構造体
 type SelectContainer struct {
 	field []string
 	table *TableContainer
@@ -13,6 +15,8 @@ type SelectContainer struct {
 	errs  []error
 }
 
+// Select
+// SelectContainerの呼び出し宣言
 func Select(fields ...string) *SelectContainer {
 	var f []string
 
@@ -26,18 +30,24 @@ func Select(fields ...string) *SelectContainer {
 	}
 }
 
+// From
+// Select文のメインテーブル宣言
 func (s *SelectContainer) From(table *TableContainer) *SelectContainer {
 	s.table = table
 
 	return s
 }
 
+// Where
+// Select文のWhere句宣言
 func (s *SelectContainer) Where(conditions *Expression) *SelectContainer {
 	s.where = conditions
 
 	return s
 }
 
+// ToSQL
+// Select文のSQL作成処理
 func (s *SelectContainer) ToSQL() (string, error) {
 	if len(s.errs) > 0 {
 		return "", errors.Join(s.errs...)
