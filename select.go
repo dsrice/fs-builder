85f80ffe81edb9e7fb8a6421bb1af88940d0cfe3
dsrice <v.fuji.0223@gmail.com>
2023-12-04 00:27:01 +0900

    change: Version1での方針変更に伴いモデル周りの対応をやめる

--- select.go
+++ select.go
@@ -3,21 +3,25 @@ package fsb
 import (
 	"errors"
 	"fmt"
-	"fsb/dataset"
-	"reflect"
 	"strings"
 )
 
 type SelectContainer struct {
 	field []string
-	table *dataset.TableContainer
+	table *TableContainer
 	errs  []error
 }
 
-func Select() *SelectContainer {
+func Select(fields ...string) *SelectContainer {
+	var f []string
+
+	if len(fields) > 0 {
+		f = fields
+	}
+
 	return &SelectContainer{
-		table: &dataset.TableContainer{},
-		field: []string{},
+		table: &TableContainer{},
+		field: f,
 	}
 }
 
@@ -27,18 +31,8 @@ func (s *SelectContainer) Field(fields ...string) *SelectContainer {
 	return s
 }
 
-func (s *SelectContainer) From(table interface{}) *SelectContainer {
-	switch t := table.(type) {
-	case dataset.Table:
-		d := table.(dataset.Table)
-		s.getStructField(d)
-		s.table.Dataset = &d
-		s.table.Name = d.TableName()
-	case string:
-		s.table.Name = t
-	default:
-		s.errs = []error{fmt.Errorf("failed set table")}
-	}
+func (s *SelectContainer) From(table TableContainer) *SelectContainer {
+	s.table = &table
 
 	return s
 }
@@ -57,25 +51,8 @@ func (s *SelectContainer) ToSQL() (string, error) {
 	}
 
 	if s.table != nil {
-		sqlElements = append(sqlElements, "FROM")
-		sqlElements = append(sqlElements, s.table.Name)
+		sqlElements = append(sqlElements, "FROM", s.table.name)
 	}
 
 	return fmt.Sprintf("%s;", strings.Join(sqlElements, " ")), nil
-}
-
-func (s *SelectContainer) getStructField(d dataset.Table) {
-	v := reflect.ValueOf(d)
-	i := reflect.Indirect(v)
-	it := i.Type()
-	nf := it.NumField()
-
-	var resultList []string
-
-	for i := 0; i < nf; i++ {
-		field := it.Field(i).Tag.Get("db")
-		resultList = append(resultList, field)
-	}
-
-	s.field = resultList
 }
